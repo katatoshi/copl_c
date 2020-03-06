0930b01c5853f5d6a8a5d3637eedcc4e529e0293
Toshifumi Katayama <kata.toshi@gmail.com>
2020-03-06 13:46:27 +0900

    functions.ml に関数を追加

--- ml4/functions.ml
+++ ml4/functions.ml
@@ -1,3 +1,10 @@
+let plus = fun x -> fun y -> x + y ;;
+
+let minus = fun x -> fun y -> x - y ;;
+
+let times = fun x -> fun y -> x * y ;;
+
+(* リストの長さを返す *)
 let rec length = fun xs -> match xs with
     [] -> 0
   | x :: xs' -> 1 + length xs' ;;
@@ -7,6 +14,7 @@ let rec range_rec = fun a -> fun b -> fun i ->
            else if b < a + i then []
                              else a + i :: range_rec a b (i + 1) ;;
 
+(* a 以上 b 以下の整数のリストを返す *)
 let range = fun a -> fun b -> range_rec a b 0 ;;
 
 let rec repeat_rec = fun x -> fun n -> fun i ->
@@ -14,6 +22,7 @@ let rec repeat_rec = fun x -> fun n -> fun i ->
            else if n < i + 1 then []
                              else x :: repeat_rec x n (i + 1) ;;
 
+(* x を n 回繰り返したリストを返す *)
 let repeat = fun x -> fun n -> repeat_rec x n 0 ;;
 
 let rec map = fun f -> fun xs -> match xs with
@@ -24,3 +33,7 @@ let rec filter = fun p -> fun xs -> match xs with
     [] -> []
   | x :: xs' -> if p x then x :: filter p xs'
                        else filter p xs' ;;
+
+let rec fold_left = fun f -> fun a -> fun xs -> match xs with
+    [] -> a
+  | x :: xs' -> fold_left f (f a x) xs' ;;
